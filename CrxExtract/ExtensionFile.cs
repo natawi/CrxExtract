c892ff1277459452daf30751798be071eda284d9
natawi <98446000+natawi@users.noreply.github.com>
2022-07-11 03:13:32 -0400

    Initial commit

---
+++ CrxExtract/ExtensionFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace CrxExtract
+{
+    internal class ExtensionFile
+    {
+        public readonly int Version;
+
+        public readonly byte[] AuthorPublicKey;
+
+        public readonly ZipArchive ZipData;
+
+        public ExtensionFile(string filename)
+        {
+            using var reader = new BinaryReader(new FileStream(filename, FileMode.Open));
+
+            if (reader.BaseStream.Length < 12)
+                throw new Exception("Invalid CRX file (invalid size)");
+
+            char[] magic = { 'C', 'r', '2', '4' };
+            if (!magic.SequenceEqual(reader.ReadChars(4)))
+                throw new Exception("Invalid CRX file (missing magic)");
+
+            Version = reader.ReadInt32();
+            if (Version != 3)
+                throw new Exception("Unknown file version");
+
+            int keySize = reader.ReadInt32();
+            if (keySize > reader.BaseStream.Length - reader.BaseStream.Position)
+                throw new Exception("Invalid author public key length");
+
+            AuthorPublicKey = reader.ReadBytes(keySize);
+
+            var zipStream = new MemoryStream(reader.ReadBytes((int)reader.BaseStream.Length - (int)reader.BaseStream.Position));
+            ZipData = new ZipArchive(zipStream, ZipArchiveMode.Read);
+        }
+    }
+}
---
+++ CrxExtract/Program.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace CrxExtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine("Extracting \"{0}\"...", arg);
+
+                ExtensionFile file;
+                try
+                {
+                    file = new ExtensionFile(arg);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("An error occurred while reading the file: {0}", exception.Message);
+                    continue;
+                }
+
+                string directoryName = arg + "_out";
+
+                ZipArchiveEntry manifest = file.ZipData.GetEntry("manifest.json");
+                if (manifest != null)
+                {
+                    var serializer = JsonSerializer.Create();
+                    using var reader = new JsonTextReader(new StreamReader(manifest.Open()));
+
+                    var json = serializer.Deserialize<JToken>(reader);
+                    if (json != null)
+                    {
+                        if (json["name"] != null)
+                            directoryName = ((string)json["name"]).Trim(Path.GetInvalidFileNameChars());
+                        else
+                            Console.WriteLine("Manifest JSON is missing a 'name' field!");
+                    }
+                    else
+                        Console.WriteLine("Manifest contains invalid JSON data!");
+                }
+                else
+                    Console.WriteLine("Odd... there appears to be no manifest!", arg);
+
+                Directory.CreateDirectory(directoryName);
+
+                foreach (ZipArchiveEntry entry in file.ZipData.Entries)
+                {
+                    string path = Path.Combine(directoryName, entry.FullName);
+                    if (path.EndsWith('\\') || path.EndsWith('/'))
+                        Directory.CreateDirectory(path);
+                    else
+                        entry.ExtractToFile(path, true);
+                } 
+            }
+        }
+    }
+}
